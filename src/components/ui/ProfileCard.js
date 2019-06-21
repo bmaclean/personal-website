329cec31d718b1fed6c5c50df5f8df64e365f22c
Brendan M <brendan.maclean94@gmail.com>
2019-06-21 16:56:09 -0600

    Replace ProfileCard CSS modules with JSS.

--- src/components/ui/ProfileCard.js
+++ src/components/ui/ProfileCard.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring';
-import './profilecard.css';
+import injectSheet from 'react-jss';
+import ProfileImage from '../../images/profile_image.png';
+
+const styles = {
+	card: {
+		width: '30ch',
+		height: '45ch',
+		background: 'grey',
+		borderRadius: '5px',
+		backgroundImage: `url(${ProfileImage})`,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center center',
+		boxShadow: '0px 10px 30px -5px rgba(0, 0, 0, 0.3)',
+		transition: 'box-shadow 0.5s',
+		willChange: 'transform',
+		border: '15px solid white'
+	}
+};
 
 const calc = (x, y) => [
 	-(y - window.innerHeight / 2) / 20,
@@ -10,14 +27,14 @@ const calc = (x, y) => [
 const trans = (x, y, s) =>
 	`perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-export default function ProfileCard() {
+function ProfileCard({classes}) {
 	const [props, set] = useSpring(() => ({
 		xys: [0, 0, 1],
 		config: {mass: 20, tension: 100, friction: 40}
 	}));
 	return (
 		<animated.div
-			class="card"
+			className={classes.card}
 			onMouseMove={({clientX: x, clientY: y}) => set({xys: calc(x, y)})}
 			onMouseLeave={() => set({xys: [0, 0, 1]})}
 			style={{
@@ -28,3 +45,5 @@ export default function ProfileCard() {
 		/>
 	);
 }
+
+export default injectSheet(styles)(ProfileCard);
