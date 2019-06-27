5c9b5cc1318e267baf0136492d476fa14f414457
Brendan M <brendan.maclean94@gmail.com>
2019-06-27 15:17:10 -0600

    Remove react-jss as styling method.

--- src/components/ui/ProfileCard.js
+++ src/components/ui/ProfileCard.js
@@ -1,23 +1,6 @@
 import React from 'react';
 import {useSpring, animated} from 'react-spring';
-import injectSheet from 'react-jss';
 import ProfileImage from '../../images/profile_image.png';
-
-const styles = {
-	card: {
-		width: '30ch',
-		height: '45ch',
-		background: 'grey',
-		borderRadius: '5px',
-		backgroundImage: `url(${ProfileImage})`,
-		backgroundSize: 'cover',
-		backgroundPosition: 'center center',
-		boxShadow: '0px 10px 30px -5px rgba(0, 0, 0, 0.3)',
-		transition: 'box-shadow 0.5s',
-		willChange: 'transform',
-		border: '15px solid white'
-	}
-};
 
 const calc = (x, y) => [
 	-(y - window.innerHeight / 2) / 20,
@@ -27,23 +10,31 @@ const calc = (x, y) => [
 const trans = (x, y, s) =>
 	`perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
-function ProfileCard({classes}) {
+export default function ProfileCard() {
 	const [props, set] = useSpring(() => ({
 		xys: [0, 0, 1],
-		config: {mass: 20, tension: 100, friction: 40}
+		config: {mass: 20, tension: 200, friction: 50}
 	}));
 	return (
 		<animated.div
-			className={classes.card}
 			onMouseMove={({clientX: x, clientY: y}) => set({xys: calc(x, y)})}
 			onMouseLeave={() => set({xys: [0, 0, 1]})}
 			style={{
 				transform: props.xys.interpolate(trans),
-				marginLeft: '52rem',
-				marginTop: '6rem'
+				width: '20vw',
+				height: '30vw',
+				background: 'grey',
+				borderRadius: '5px',
+				backgroundImage: `url(${ProfileImage})`,
+				backgroundSize: 'cover',
+				backgroundPosition: 'center center',
+				boxShadow: '20px 20px 50px -5px rgba(0, 0, 0, 0.7)',
+				transition: 'box-shadow 0.5s',
+				willChange: 'transform',
+				// TODO: margins should be set from Greeting view
+				// marginLeft: '65vw',
+				marginTop: '16vh'
 			}}
 		/>
 	);
 }
-
-export default injectSheet(styles)(ProfileCard);
