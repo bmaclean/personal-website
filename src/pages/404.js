3f5a77ff56b83dde46b8a0e4ead41d17c40bb91e
Brendan M <brendan.maclean94@gmail.com>
2019-07-03 16:59:01 -0600

    Simple implementation of 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,41 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import {useTheme} from '../hooks';
+import {Title} from '../components/ui';
 
-const NotFoundPage = () => (
-	<Layout>
-		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
-		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-	</Layout>
-);
+export default function NotFoundPage() {
+	const theme = useTheme();
+	return (
+		<Layout>
+			<div
+				css={{
+					position: 'absolute',
+					height: '100%',
+					width: '100%',
+					background: theme.colors.background
+				}}
+			>
+				<SEO title="404: Not found" />
+				<div
+					css={{
+						marginTop: '10%',
+						marginLeft: '10%'
+					}}
+				>
+					<Title variant="page">Not found.</Title>
+					<Title
+						css={{
+							marginTop: '1rem'
+						}}
+						variant="subtitle"
+					>
+						Double check the URL and try again.
+					</Title>
+				</div>
 
-export default NotFoundPage;
+				<p></p>
+			</div>
+		</Layout>
+	);
+}
