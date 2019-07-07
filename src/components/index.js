a0fecff58fecd6136ac4ce8d8bf435387f7f887f
Brendan M <brendan.maclean94@gmail.com>
2019-07-07 02:35:24 -0600

    Update fonts

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
-export {default as PaperForm} from './PaperForm';
+export {default as Card} from './Card';
 export {default as Layout} from './Layout';
-export {default as SEO} from './SEO';
+export {default as PaperForm} from './PaperForm';
 export {default as ProfileCard} from './ProfileCard';
+export {default as SEO} from './SEO';
+export {default as ServiceCard} from './ServiceCard';
 export {default as Title} from './Title';
 export {default as ViewLayer} from './ViewLayer';
