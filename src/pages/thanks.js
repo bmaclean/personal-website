6fa390d1dd0a6793ebd4a1277a1cabe27831ccdc
brendanmaclean <brendan.maclean@alumni.ubc.ca>
2019-07-13 09:36:32 -0600

    Basic Thanks landing page

---
+++ src/pages/thanks.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import {Link} from 'gatsby';
+
+import {Layout, SEO} from '../components';
+
+const Thanks = () => {
+	return (
+		<Layout>
+			<SEO title="Thanks" />
+			<p>
+				Thanks!
+			</p>
+		</Layout>
+	);
+};
+
+export default Thanks;
