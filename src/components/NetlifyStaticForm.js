3c82fcf9e1ff9299d7594cb73847deed4fc6d77b
Brendan M <brendan.maclean94@gmail.com>
2019-07-14 22:46:25 -0600

    Hide static form

--- src/components/NetlifyStaticForm.js
+++ src/components/NetlifyStaticForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NetlifyStaticForm() {
 	return (
-		<form method="post" data-netlify="true" name="contact">
+		<form method="post" data-netlify="true" name="contact" css={{display: 'none'}}>
 			<input type="hidden" name="form-name" value="contact" />
 			<input type="text" name="name" id="name"  />
 			<input type="text" name="email" id="email"  />
