816b2672a4ae79d56683055d1b3cb532b0def060
Brendan Maclean <brendan@ignitertickets.com>
2020-02-24 08:08:34 -0700

    Remove unecessary compoenents from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,9 @@
 import React, {useState} from 'react';
-// import {Link} from 'gatsby';
-import {Parallax} from 'react-spring/renderprops-addons';
 
-import {Toast, Layout, NetlifyStaticForm, SEO} from '../components';
-import {Contact, Greeting, Services} from '../views';
+import {Layout, NetlifyStaticForm, SEO} from '../components';
 import Logo from '../images/temp-logo.png';
 
 const IndexPage = () => {
-	const [alertOpen, setAlertOpen] = useState(false);
-
 	return (
 		<Layout>
 			<SEO image={Logo} title="Home" />
